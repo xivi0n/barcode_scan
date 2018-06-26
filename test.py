b51f287378d69378ab585c3cc113898f8cd3b579
Mihailo Jovanovic <mihailo.jovanovich@gmail.com>
2018-06-26 18:56:19 +0200

    Guessing unkown values; optimization 65%

--- test.py
+++ test.py
@@ -8,12 +8,16 @@ start = False # true if mouse is pressed
 ix,iy = -20,-20
 res = 32
 base = cv2.imread(file_name)
-numbers = [[3,2,1,1],
-           [2,2,2,1],[2,1,2,2],
-           [1,4,1,1],[1,1,3,2],
-           [1,2,3,1],[1,1,1,4],
-           [1,3,1,2],[1,2,1,3],
-           [3,1,1,2]]
+numbers = [[3,2,1,1],#0
+           [2,2,2,1],#1
+           [2,1,2,2],#2
+           [1,4,1,1],#3
+           [1,1,3,2],#4
+           [1,2,3,1],#5
+           [1,1,1,4],#6
+           [1,3,1,2],#7
+           [1,2,1,3],#8
+           [3,1,1,2]]#9
 read = []
 read_n = []
 tocmp = [] 
@@ -30,6 +34,7 @@ dx = 0
 px1 = 0
 px2 = 0
 px3 = 0
+again = False
 
 def draw_rec(x,y):
     global ix,iy,res
@@ -93,7 +98,7 @@ def make_n():
             read_n.append(pom)
             pom=[]
     #print read
-    print read_n
+    #print read_n
 
 def remake_n():
     global px1,px2,px3
@@ -114,6 +119,16 @@ def remake_n():
             px = px2-px3
             px /=4
 
+def calcxpx(x,k):
+    if (1.0*x<=1):
+        return 1
+    elif(10.0*x%10<k):
+        return (math.trunc(1.0*x))
+    elif(10.0*x%10>10-k):
+        return (math.trunc(1.0*x)+1)
+    else:
+        return ("NE")
+
 def testtest():
     for aje in read_n:
         px = 0
@@ -128,52 +143,73 @@ def testtest():
             k+=1
             if (1.0*x/px<=1):
                 pom.append(1)
-                pom2.append(1)
-            elif(10.0*x/px%10<3):
-                pom2.append(math.trunc(1.0*x/px))
-                pom.append(math.trunc(1.0*x/px))
-            elif(10.0*x/px%10>7):
-                pom2.append(math.trunc(1.0*x/px)+1)
-                pom.append(math.trunc(1.0*x/px)+1)
             else:
                 pom.append(1.0*x/px)
-                pom2.append("NE")
-
+            pom2.append(calcxpx(1.0*x/px,2))
         print px,aje,pom2,pom
         tocmp.append(pom)
         flag.append(pom2)
-    testtest2()
+    
+
+    while(testtest2()!=0):
+        print "PONOVO"
+    print 
+    print flag
     print 
 
+def onek(m):
+    k = 0
+    for i in range(0,4):
+        if (flag[m][i]!="NE"):
+            k+=flag[m][i]
+        else:
+            j = i
+    flag[m][j]=7-k
+
 def testtest2():
+    global again
+    again = False
+    numk = 0
     for m in range(0,len(flag)):
         k = 0
         sum = 0
+        n = 0
+        ktor=[]
         for i in range(0,4):
             if (flag[m][i]=="NE"):
+                ktor.append(i)
                 k+=1
             elif (flag[m][i]>4):
                 flag[m][i]=4
-                tocmp[m][i]=4
+                #tocmp[m][i]=4
+            
+            if (flag[m][i]==4) and ((i==0) or (i==2)):
+                flag[m][i]=3
 
             if (flag[m][i]!="NE"):
                 sum += flag[m][i]
-            
         if (k==1):
-            k = 0
-            for i in range(0,4):
-                if (flag[m][i]!="NE"):
-                    k+=flag[m][i]
-                else:
-                    j = i
-            print 7-k,
-            print m,j
-            tocmp[m][j]=7-k
-        elif(k>1) and (k!=4):
+            onek(m)
+            k-=1
+            #tocmp[m][j]=7-k
+        elif(k>1):
+            min = abs(tocmp[m][ktor[0]]%1-0.50)
+            for i in range(1,len(ktor)):
+                pom = abs(tocmp[m][ktor[0]]%1-0.50)
+                n = ktor[0]
+                if (pom>min):
+                    min = pom
+                    n = ktor[i]
+            k-=1
+            flag[m][n] = calcxpx(tocmp[m][n],5)
+            again = True
+        elif (k!=0):
             print "jbg vise NE"
-
-
-    print tocmp
+        numk+=k
+    return numk
+        
+    #if again:
+     #  testtest2()
 
 def cmparr(a,b):
     pom = 0
@@ -183,7 +219,7 @@ def cmparr(a,b):
     return pom
 
 def cmpr():
-    for x in tocmp:
+    for x in flag:
         print "poco",
         for y in numbers:
             if (cmparr(x,y)==4):
@@ -229,5 +265,8 @@ calc_px()
 make_n()
 #remake_n()
 testtest()
+
+
 cmpr()
+
 cv2.destroyAllWindows()
